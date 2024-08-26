c3b8ec452f4f3783621a9c2e92ed0d11d3c9b995
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-08-26 18:27:15 +0700

    update CounterTest.php test dependency

--- tests/CounterTest.php
+++ tests/CounterTest.php
@@ -13,9 +13,9 @@
       $counter->increment();
       Assert::assertEquals(1, $counter->getCounter());
       $counter->increment();
-      Assert::assertEquals(2, $counter->getCounter());
+      $this->assertEquals(2, $counter->getCounter());
       $counter->increment();
-      Assert::assertEquals(3, $counter->getCounter());
+      self::assertEquals(3, $counter->getCounter());
     }
 
     /**
@@ -26,5 +26,22 @@
       $counter = new Counter();
       $counter->increment();
       $this->assertEquals(1, $counter->getCounter());
+    }
+
+    public function testFirst(): Counter
+    {
+      $counter = new Counter();
+      $counter->increment();
+      $this->assertEquals(1, $counter->getCounter());
+      return $counter;
+    }
+
+    /**
+    * @depends testFirst
+    */
+    public function testSecond(Counter $counter)
+    {
+      $counter->increment();
+      $this->assertEquals(2, $counter->getCounter());
     }
   }
